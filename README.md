0c133516dda05d3a439da420e23e34ac5448de60
Roni Riman <rimanroni386@gmail.com>
2024-07-14 02:00:05 +0600

    full projects done

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# React + Vite
+last update 14/7/2024 
+just learning and pactics  projects
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+rimanroni386@gmail.com
+for any message please contact me 
+https://roni-riman-portfolio.vercel.app
--- src/pages/Order/MyOrder.jsx
+++ src/pages/Order/MyOrder.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../shared/Navbar/Navbar';
 import { MyContaxt } from '../../Contax/Contax';
 import Footer from '../../shared/Footer/Footer';
 import { Helmet } from 'react-helmet';
+import Swal from 'sweetalert2';
 
 const MyOrder = () => {
     const {user , loading} = useContext(MyContaxt);
@@ -15,6 +16,36 @@ const MyOrder = () => {
     if(loading){
         return <div className='h-[60vh] flex justify-center items-center'><p>Loading Data Please Waiting...</p></div>
     }
+    const handleDelet = (id) =>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Your Order Delete!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/order/${id}`,{
+                    method:"DELETE"
+                    
+                })
+                .then(res=>res.json())
+                .then(datas=>{
+                      const updateData = data.filter(item=>item._id !==id);
+                     setData(updateData)
+                });
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          });
+
+       
+    }
     return (
         <div>
             <Helmet>
@@ -25,7 +56,7 @@ const MyOrder = () => {
                 {data.map((item)=>(
                     <div key={item._id} className='flex items-center justify-between border  px-4'>
                         <div className='h-[200px]  flex justify-center items-center space-x-4 px-4'>
-                            <button className='bg-primaryColor px-2  text-white font-bold rounded-full'>
+                        <button onClick={()=>handleDelet(item._id)} className='bg-primaryColor px-2  text-white font-bold rounded-full'>
                             <h1 className=' '>X</h1>
                             </button>
                             <img src={item.img} alt="" className='w-[200px]'/>
